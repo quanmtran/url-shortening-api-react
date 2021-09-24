61cd40f3f1f2e771cb6cfb89fcdbe719403760c7
Quan Tran <quantm.cn@gmail.com>
2021-09-25 02:14:43 +1000

    Fix icons

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export default function Card(props) {
-	const { url, heading, text } = props;
-	
+	const { heading, text } = props;
+
 	return (
 		<div className="card">
-			<div className="card-icon" style={{ backgroundImage: `url(${url})` }}></div>
+			<div className="card-icon"></div>
 			<div className="heading">{heading}</div>
 			<div className="gray-text">{text}</div>
 		</div>
